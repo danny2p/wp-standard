9bf37b7715463550f23172f77d322e8b088610cf
Danny Pfeiffer <danny.pfeiffer@pantheon.io>
2022-02-26 18:03:54 -0700

    trying foreground flag to see if we'll get realtime output within timeout

--- .github/scripts/shell/timeout-deploy.sh
+++ .github/scripts/shell/timeout-deploy.sh
@@ -5,5 +5,8 @@
 # Get paths
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+SITE=$1
+echo -e "Starting ${SITE}";
+
 # Timeout after 15 minutes.
-timeout 15m ${__dir}/deploy-sequence.sh $1
+timeout --foreground 15m ${__dir}/deploy-sequence.sh $1
