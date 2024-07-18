305f02ba2de74dd303d4ec983072c217ba00e8a9
Danny P <danny.pfeiffer@pantheon.io>
2024-07-17 22:54:34 -0600

    Adding slack notifications back in

--- .github/scripts/dev-deploy.sh
+++ .github/scripts/dev-deploy.sh
@@ -13,11 +13,11 @@ START=$SECONDS
 # Tell slack we're starting this site
 SLACK_START="Started ${SITE} deployment to Dev"
 
-#curl -X POST -H 'Content-type: application/json' --data "{'text':'${SLACK_START}'}" $SLACK_WEBHOOK
+curl -X POST -H 'Content-type: application/json' --data "{'text':'${SLACK_START}'}" $SLACK_WEBHOOK
 echo -e "Starting ${SITE}";
 
 # Backup DB prior to deploy, 30 day retention
-terminus backup:create --element database --keep-for 30 -- $SITE.dev
+# terminus backup:create --element database --keep-for 30 -- $SITE.dev
 
 # Check site upstream for updates, apply
 # terminus site:upstream:clear-cache $1 -q
@@ -33,7 +33,7 @@ terminus upstream:updates:apply $DEV --accept-upstream -q
 # terminus upstream:updates:apply --updatedb --accept-upstream -- <site>.<env>
 
 SLACK="${SITE} DEV Code Deployment Finished. Importing config and clearing cache."
-#curl -X POST -H 'Content-type: application/json' --data "{'text':'${SLACK}'}" $SLACK_WEBHOOK
+curl -X POST -H 'Content-type: application/json' --data "{'text':'${SLACK}'}" $SLACK_WEBHOOK
 
 # Run any post-deploy commands here
 terminus env:clear-cache $SITE.dev
@@ -44,5 +44,5 @@ TIME_DIFF=$(bc <<< "scale=2; $DURATION / 60")
 MIN=$(printf "%.2f" $TIME_DIFF)
 SITE_LINK="https://dev-${SITE}.pantheonsite.io";
 SLACK=":white_check_mark: Finished ${SITE} deployment to Dev in ${MIN} minutes. \n ${SITE_LINK}"
-#curl -X POST -H 'Content-type: application/json' --data "{'text':'${SLACK}'}" $SLACK_WEBHOOK
+curl -X POST -H 'Content-type: application/json' --data "{'text':'${SLACK}'}" $SLACK_WEBHOOK
 
