a9c8d5f9ea03f4928030f4ff035dfb1105e5dac3
Danny Pfeiffer <danny.pfeiffer@pantheon.io>
2024-12-04 10:20:37 -0700

    Some new left rail features and styles

--- .github/scripts/test-deploy.sh
+++ .github/scripts/test-deploy.sh
@@ -40,7 +40,6 @@ DURATION=$(( SECONDS - START ))
 TIME_DIFF=$(bc <<< "scale=2; $DURATION / 60")
 MIN=$(printf "%.2f" $TIME_DIFF)
 echo -e "Finished ${SITE} in ${MIN} minutes"
-echo "${SITE},${ID},${MIN}" >> /tmp/results.txt
 
 SITE_LINK="https://test-${SITE}.pantheonsite.io";
 SLACK=":white_check_mark: Finished ${SITE_LABEL} full deployment in ${MIN} minutes. \n ${SITE_LINK}"
